f3416e564b8583da97638ecf02852c180a037151
David Cary <d.cary@ieee.org>
2025-07-30 16:08:04 -0700

    Update embed_quine.c

--- embed_quine.c
+++ embed_quine.c
@@ -14,14 +14,23 @@ https://thephd.dev/finally-embed-in-c23#actual-real-touchable-non-electronic-mai
 According to
 https://gcc.gnu.org/projects/c-status.html
 , C23 mode is the default since GCC 15.
+
+According to
+"How to implement C23 #embed in GCC 15"
+https://developers.redhat.com/articles/2025/01/30/how-implement-c23-embed-gcc-15#4_methods_to_add_binary_data
+, "GCC 15 is expected to be released in April or May 2025."
 */
 
 #include <stdio.h> // for printf()
 
+#ifdef __has_embed
 char source_code[] = {
     #embed "embed_quine.c"
     , 0 // add a null terminator
 };
+#else
+#error "requires the C23 feature '#embed' ."
+#endif
 
 int main(void){
     printf("%s", source_code);
