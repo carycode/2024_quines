f501780df702d8fe35e9d9893d297000fb64c2c0
David Cary <d.cary@ieee.org>
2026-03-03 19:30:47 -0800

    compiles and runs, but not yet correct

--- list_of_strings_quine.c
+++ list_of_strings_quine.c
@@ -2,6 +2,7 @@
 
 // list_of_strings_quine.c
 // version 0.2026.01
+// 2026-03-03: compiles and runs cleanly, but not yet self-regenerating.
 // 2026-03-02: started by David Cary (forked from my extern_quine.c )
 // Unlike most quines, this is:
 // * ANSI C compatible (in particular, it does *not* depend on ASCII)
@@ -23,13 +24,13 @@ int main(void){
     // and printing a special character (which typically ends up being a newline)
     // FUTURE: support empty strings (so we can emit 2 consecutive special characters)
     // FUTURE: consider supporting "empty" special character strings (so we emit 2 consecutive normal strings)
-    for(int i = 6; d[i][0]; i++){
+    for(int i = 7; d[i][0]; i++){
         // first character in every string is a digit representing a special character
         char *s = d[i];
-        printf("%s", s+1); // skip over the special
+        // printf("%s", s+1); // skip over the special
         printf(d[4], s+1); // skip over the special
         int special = (s[0] - '0');
-        printf("%s", d[special]); // newline, quote, or backslash
+        // printf("%s", d[special]); // newline, quote, or backslash
         printf(d[4], d[special]); // newline, quote, or backslash
     };
 
@@ -48,13 +49,14 @@ int main(void){
         i++;
         char *s = d[i];
         putchar('"');
-        printf( "%s", s );
+        // printf( "%s", s );
+        printf( d[4], s );
         puts( "\","); // end-quote, comma, and also emits newline
     }while(d[i][0]);
 #endif
 
     // emit ending bracket of list and post-string executable 
-    puts( "};" );
+    // puts( "};" );
     puts( d[5] );
 }
 
@@ -63,7 +65,11 @@ char *d[]={
 // start with a teleomere of bytes we need that must be escaped
 "", // 0 empty string -- perhaps unnecessary?
 "\n", // 1 newline
+#if 1
 "\"", // 2 quote
+#else
+{ '"', 0 }, // 2 quote
+#endif
 "\\", // 3 backslash
 // from here on, we avoid using escape sequences ...
 "%s", // 4
@@ -71,29 +77,40 @@ char *d[]={
 // citation and attribution (analogous to junk dna?)
 " 2025-07-10: ANSI C quine by David Cary", // 6
 // then more-or-less the text of the executable quine
-"1#include <stdio.h>Z0", // 6
-"1extern char d[];Z0",
-"1int main(void){Z0",
-"1    for(int i = 43; d[i]; i++){Z0",
-"1        char c = d[i];Z0",
-"1        if('Z4' == c){Z0",
-"1            i++;Z0",
-"1            c = d[i] - '0';Z0",
-"1            putchar(d[c]);Z0",
-"1        }else{Z0",
-"1            putchar(c);Z0",
-"1        };Z0",
-"1    };Z0",
-"1    puts(d+3);Z0",
-"1    puts( Z2Z1Z2;Z2 );Z0",
-"1}Z0",
-"1char d[]=Z0Z2",
+"1#include <stdio.h>", // 7
+"1extern char *d[];",
+"1int main(void){",
+"1    for(int i=7; d[i][0]; i++){",
+"1        char *s = d[i];",
+"1        printf(d[4], s+1);",
+"1        int x = s[0] - '0';",
+"1        printf(d[4], d[x]);",
+"1    };",
+"1    for(int i=4; d[i][0]; i++){",
+"1        char *s = d[i];",
+"2        putchar('",
+"1');",
+"1        printf( d[4], s );",
+"2        puts(",
+"3",
+"2",
+"2,",
+"1);",
+"1    };",
+"1    puts(d[5]);",
+"1}",
+"1char *d[]={",
 // then reconstruct the above teleomere
 "Z1Z3",
 "Z1Z1",
 "Z1Z2",
+#if 1
 // empty string marks end-of-list:
 ""
+#else
+// null pointer marks end-of-list:
+0
+#endif
 // (end-of-list is effectively the self-referential particle)
 // then reconstruct the post-teleomere list of strings
 // and this final "};\n".
